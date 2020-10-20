559ac7aa17e19391729869a53bad50b3dc17bd79
ovidiunegru <44364275+ovidiunegru@users.noreply.github.com>
2020-10-20 14:38:43 +0300

    Homework day 2.
    Added layout for the restaurant app item, created an intent from From SplashActivity to RestaurantsActivity and a package restaurants for RestaurantsActivity.java.

---
+++ app/src/main/java/com/example/restaurantaplication/SplashActivity.java
@@ -0,0 +1,20 @@
+package com.example.restaurantaplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.restaurantaplication.restaurants.RestaurantsActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(getApplicationContext(), RestaurantsActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/restaurantaplication/restaurants/RestaurantsActivity.java
@@ -0,0 +1,17 @@
+package com.example.restaurantaplication.restaurants;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restaurantaplication.R;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+
+    }
+}
